f7cf66f7e5f7b5016d71b61f4b5b6c49e29f4d97
Sumner1185 <alastair.sumner@yahoo.com>
2020-06-15 14:46:09 +0100

    First rspec test written

---
+++ spec/listing_spec.rb
@@ -0,0 +1,17 @@
+require 'listing'
+
+describe Listing do
+  
+  describe '.add' do
+  
+    it 'should add a new listing' do
+      listing = Listing.add(title: 'Test listing', description: 'Amazing place', price: 100, location: 'N1 4RL')
+
+      expect(listing).to be_a Listing
+      expect(listing.title).to eq 'Test listing'
+      expect(listing.price).to eq 100
+    end
+
+  end
+
+end
