05eec447729145b73382bb06f7d6cf97388fe0bc
Sophie Worsnop <sophie.g.worsnop@gmail.com>
2020-06-15 14:43:48 +0100

    Views created and spec helper updated - [views branch]

--- app.rb
+++ app.rb
@@ -1,13 +1,24 @@
 require 'sinatra/base'
 require './lib/listing.rb'
 
-
 class Makersbnb < Sinatra::Base
 
+get '/' do
+ erb :homepage
+end
+
+get '/add-listing' do
+  
+end
 
 
+# get '/login' do
 
+# end
 
+# get '/sign-in' do
+  
+# end
 
 
 run! if app_file == $0
