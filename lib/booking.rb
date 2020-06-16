14f011c72dc1e94ca66b36daecd9180b85dc9765
Sumner1185 <alastair.sumner@yahoo.com>
2020-06-16 18:33:04 +0100

    Refactor class with private method for database connection

--- lib/booking.rb
+++ lib/booking.rb
@@ -10,16 +10,22 @@ class Booking
   end
 
   def self.add(date:, listingid:)
-    if ENV['ENVIRONMENT'] == 'test'
-      @con = PG.connect(dbname: 'makersbnb_test')
-    else
-     @con = PG.connect(dbname: 'makersbnb')
-    end
+    database_connection
 
     result = @con.exec("INSERT INTO booking (date, listingid) 
     VALUES ('#{date}', '#{listingid}')
     RETURNING bookingid, date, listingid;")
     Booking.new(id: result[0]['bookingid'], date: result[0]['date'], listingid: (result[0]['listingid']).to_i)
+  end
+
+  private
+
+  def self.database_connection
+    if ENV['ENVIRONMENT'] == 'test'
+      @con = PG.connect(dbname: 'makersbnb_test')
+    else
+     @con = PG.connect(dbname: 'makersbnb')
+    end
   end
 
 end
