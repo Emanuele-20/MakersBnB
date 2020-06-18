6dead10c998bab8a290af86bfc2dd8cc9c7660c8
Sumner1185 <alastair.sumner@yahoo.com>
2020-06-18 10:36:29 +0100

    Update listing db creation info

--- db/01_create_listing_table.sql
+++ db/01_create_listing_table.sql
@@ -1,7 +1,10 @@
 CREATE TABLE listing (
-    listingid SERIAL PRIMARY KEY, 
-    title VARCHAR(50), 
-    description VARCHAR(200), 
-    price INT, 
-    postcode TEXT
+  listingid SERIAL PRIMARY KEY, 
+  title VARCHAR(50), 
+  description VARCHAR(200), 
+  price INT, 
+  postcode TEXT,
+  userid INT,
+  CONSTRAINT FK_userid FOREIGN KEY (userid)
+  REFERENCES users(userid)
 );
